97faa7812529c46283f87f9b965eb0f9d528ef28
cgiustiCreek <cgiusti@webcreek.com>
2020-06-16 11:42:56 -0500

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data{
+    public class DataContext : DbContext{
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options){
+         
+        }
+    }
+}
